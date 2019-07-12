3c40a4bc0c1722c0eab7632d0a254ca618f9983f
LuisOR64 <luis.ta.knt@gmail.com>
2019-07-13 03:35:14 +0900

    uso de google maps api
    google.maps.Marker
    navigator.geolocation
    setCenter
    setZoom
    type="module"
    import JS
    export JS

---
+++ script/main.js
@@ -0,0 +1,48 @@
+import {styles_map} from './variable.js';
+$(()=>{
+    var map;
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+       center: {lat: -16.4302734, lng: -71.5190854},
+       zoom: 8,
+       styles: styles_map
+    });
+  
+    var get_position = ()=>{
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position=>{
+          console.log(position);
+          var {latitude, longitude} = position.coords;
+
+          // var uluru = {lat: -25.344, lng: 131.036};
+  
+          var marker = new google.maps.Marker(
+            {position: {
+                lat: latitude, 
+                lng: longitude
+            },
+             map: map
+            });
+            console.log(marker);
+
+            map.setCenter({lat: latitude, lng: longitude});
+
+            map.setZoom(8);
+
+
+            console.log(latitude);
+            console.log(longitude);
+
+        }, position=>{
+          console.error(position);
+          // alert("User has denegated the geolocalization");
+          $.notify("User has denegated the geolocalization", "error");
+        });
+      }else{
+        alert("This browser don't support geolocalization");
+      }
+    }
+
+    get_position();
+
+})
---
+++ script/variable.js
@@ -0,0 +1,22 @@
+// para indicar que esta variable va a ser exportada
+export var styles_map=
+[
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#0000ff"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#00ff00"
+        }
+      ]
+    }
+  ];
